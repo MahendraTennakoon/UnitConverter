3f46c8efb4d786a0a073c7b28ff8f4f7971dbf16
Mahendra Tennakoon <mahendrathennakoon@gmail.com>
2018-08-19 00:19:43 +0530

    Convert pounds and ounces

--- UnitConverter/FirstViewController.swift
+++ UnitConverter/FirstViewController.swift
@@ -5,9 +5,15 @@ class FirstViewController: UIViewController {
     
     @IBOutlet weak var gramsField: UITextField!
     @IBOutlet weak var kilogramsField: UITextField!
+    @IBOutlet weak var poundsField: UITextField!
+    @IBOutlet weak var ouncesField: UITextField!
+    
     // weight in grams
     var weight = 0.0
     
+    let gramsInKilogram:Double = 1000.0
+    let gramsInPound:Double = 453.592
+    let gramsInOunce:Double = 28.3495
     
     @IBAction func updateWeight(_ sender: UITextField) {
         let value = Double(sender.text!)
@@ -17,7 +23,13 @@ class FirstViewController: UIViewController {
             weight = value!
             break
         case kilogramsField:
-            weight = value! * 1000
+            weight = value! * gramsInKilogram
+            break
+        case poundsField:
+            weight = value! * gramsInPound
+            break
+        case ouncesField:
+            weight = value! * gramsInOunce
             break
         default:
             weight = 0
@@ -25,10 +37,12 @@ class FirstViewController: UIViewController {
         
         self.updateUI()
     }
-        
+    
     func updateUI() {
         gramsField.text = String(weight)
-        kilogramsField.text = String(weight / 1000)
+        kilogramsField.text = String(weight / gramsInKilogram)
+        poundsField.text = String(weight / gramsInPound)
+        ouncesField.text = String(weight / gramsInOunce)
     }
 }
 
