34e7392064649fae578f995631c76343f252c329
Mahendra Tennakoon <mahendrathennakoon@gmail.com>
2018-08-19 09:41:43 +0530

    Guard for speed conversion

--- UnitConverter/SpeedViewController.swift
+++ UnitConverter/SpeedViewController.swift
@@ -10,7 +10,9 @@ class SpeedViewController: UIViewController {
     
     
     @IBAction func updateValue(_ sender: UITextField) {
-        let value = Double(sender.text!)
+        guard let value = Double(sender.text!) else {
+            return
+        }
         
         let converter = UnitConvert(strategy: SpeedConversionStrategy())
         var metresPerSec: Double = 0
@@ -20,28 +22,28 @@ class SpeedViewController: UIViewController {
         
         switch sender {
         case metresPerSecField:
-            metresPerSec = value!
-            feetPerMin = converter.convert(unitFrom: Speed.MetrePerSec.rawValue, unitTo: Speed.FeetPerMinute.rawValue, value: value!)
-            kilometresPerHour = converter.convert(unitFrom: Speed.MetrePerSec.rawValue, unitTo: Speed.KilometrePerHour.rawValue, value: value!)
-            milesPerSec = converter.convert(unitFrom: Speed.MetrePerSec.rawValue, unitTo: Speed.MilesPerHour.rawValue, value: value!)
+            metresPerSec = value
+            feetPerMin = converter.convert(unitFrom: Speed.MetrePerSec.rawValue, unitTo: Speed.FeetPerMinute.rawValue, value: value)
+            kilometresPerHour = converter.convert(unitFrom: Speed.MetrePerSec.rawValue, unitTo: Speed.KilometrePerHour.rawValue, value: value)
+            milesPerSec = converter.convert(unitFrom: Speed.MetrePerSec.rawValue, unitTo: Speed.MilesPerHour.rawValue, value: value)
             break
         case feetPerMinField:
-            metresPerSec = converter.convert(unitFrom: Speed.FeetPerMinute.rawValue, unitTo: Speed.MetrePerSec.rawValue, value: value!)
-            feetPerMin = value!
-            kilometresPerHour = converter.convert(unitFrom: Speed.FeetPerMinute.rawValue, unitTo: Speed.KilometrePerHour.rawValue, value: value!)
-            milesPerSec = converter.convert(unitFrom: Speed.FeetPerMinute.rawValue, unitTo: Speed.MilesPerHour.rawValue, value: value!)
+            metresPerSec = converter.convert(unitFrom: Speed.FeetPerMinute.rawValue, unitTo: Speed.MetrePerSec.rawValue, value: value)
+            feetPerMin = value
+            kilometresPerHour = converter.convert(unitFrom: Speed.FeetPerMinute.rawValue, unitTo: Speed.KilometrePerHour.rawValue, value: value)
+            milesPerSec = converter.convert(unitFrom: Speed.FeetPerMinute.rawValue, unitTo: Speed.MilesPerHour.rawValue, value: value)
             break
         case kilometresPerHourField:
-            metresPerSec = converter.convert(unitFrom: Speed.KilometrePerHour.rawValue, unitTo: Speed.MetrePerSec.rawValue, value: value!)
-            feetPerMin = converter.convert(unitFrom: Speed.KilometrePerHour.rawValue, unitTo: Speed.FeetPerMinute.rawValue, value: value!)
-            kilometresPerHour = value!
-            milesPerSec = converter.convert(unitFrom: Speed.KilometrePerHour.rawValue, unitTo: Speed.MilesPerHour.rawValue, value: value!)
+            metresPerSec = converter.convert(unitFrom: Speed.KilometrePerHour.rawValue, unitTo: Speed.MetrePerSec.rawValue, value: value)
+            feetPerMin = converter.convert(unitFrom: Speed.KilometrePerHour.rawValue, unitTo: Speed.FeetPerMinute.rawValue, value: value)
+            kilometresPerHour = value
+            milesPerSec = converter.convert(unitFrom: Speed.KilometrePerHour.rawValue, unitTo: Speed.MilesPerHour.rawValue, value: value)
             break
         case milesPerHourField:
-            metresPerSec = converter.convert(unitFrom: Speed.MilesPerHour.rawValue, unitTo: Speed.MetrePerSec.rawValue, value: value!)
-            feetPerMin = converter.convert(unitFrom: Speed.MilesPerHour.rawValue, unitTo: Speed.FeetPerMinute.rawValue, value: value!)
-            kilometresPerHour = converter.convert(unitFrom: Speed.MilesPerHour.rawValue, unitTo: Speed.KilometrePerHour.rawValue, value: value!)
-            milesPerSec = value!
+            metresPerSec = converter.convert(unitFrom: Speed.MilesPerHour.rawValue, unitTo: Speed.MetrePerSec.rawValue, value: value)
+            feetPerMin = converter.convert(unitFrom: Speed.MilesPerHour.rawValue, unitTo: Speed.FeetPerMinute.rawValue, value: value)
+            kilometresPerHour = converter.convert(unitFrom: Speed.MilesPerHour.rawValue, unitTo: Speed.KilometrePerHour.rawValue, value: value)
+            milesPerSec = value
             break
         default:
             metresPerSec = 0
